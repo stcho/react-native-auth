82656391350505e6b712f2700b85dc87471a34c2
Thomas Cho <s.thomas.cho@gmail.com>
2018-03-10 14:49:04 -0700

    Custom Input component for login form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,22 +1,29 @@
 import React, { Component } from 'react';
-import { TextInput } from 'react-native';
-import { Button, Card, CardSection } from './common'
+import { Button, Card, CardSection, Input } from './common'
 
 class LoginForm extends Component {
-	state = { text: '' }
+	state = { email: '', password: '' }
 
 	render() {
 		return (
 			<Card>
 				<CardSection>
-					<TextInput 
-						value={this.state.text}
-						onChangeText={text => this.setState({ text: text })}
+					<Input 
+						placeholder="user@gmail.com"
+						label="Email"
+						value={this.state.email}
+						onChangeText={text => this.setState({ email: text })}
 					/>
 				</CardSection>
 
 				<CardSection>
-					
+					<Input 
+						placeholder="password"
+						label="Password"
+						value={this.state.password}
+						onChangeText={text => this.setState({ password: text })}
+						secureTextEntry
+					/>
 				</CardSection>
 
 				<CardSection>

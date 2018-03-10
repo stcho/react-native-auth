564e7bbf3c83b97d7cb542a81f03872d06082df0
Thomas Cho <s.thomas.cho@gmail.com>
2018-03-10 12:57:30 -0700

    Login form and initial Input component

---
+++ src/components/LoginForm.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { TextInput } from 'react-native';
+import { Button, Card, CardSection } from './common'
+
+class LoginForm extends Component {
+	state = { text: '' }
+
+	render() {
+		return (
+			<Card>
+				<CardSection>
+					<TextInput 
+						value={this.state.text}
+						onChangeText={text => this.setState({ text: text })}
+					/>
+				</CardSection>
+
+				<CardSection>
+					
+				</CardSection>
+
+				<CardSection>
+					<Button>
+						Log In
+					</Button>
+				</CardSection>
+			</Card>
+		);
+	}
+}
+
+export default LoginForm;
